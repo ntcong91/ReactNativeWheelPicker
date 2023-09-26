477c57beed6175f6afc53d66daabeee09849a501
ntcong91 <tricong09t3@gmail.com>
2023-09-26 15:36:20 +0700

    Update WheelPicker.ios.tsx

--- src/WheelPicker.ios.tsx
+++ src/WheelPicker.ios.tsx
@@ -1,6 +1,6 @@
+import { Picker } from "@react-native-picker/picker";
 import React, { useState } from "react";
 import { View } from "react-native";
-import { Picker } from "@react-native-community/picker";
 
 interface Props {
   data: Array<string>;
@@ -12,9 +12,9 @@ interface Props {
 const WheelPicker: React.FC<Props> = props => {
   const [selectedItem, setSelectedItem] = useState(props.selectedItem || 0);
   const { data, onItemSelected, disabled } = props;
+  
   if (!data || data.length === 0) return null;
   return (
-    <View pointerEvents={disabled ? "none" : "auto"}>
       <Picker
         {...props}
         selectedValue={data[selectedItem]}
@@ -27,7 +27,6 @@ const WheelPicker: React.FC<Props> = props => {
           <Picker.Item key={index} label={i} value={i} />
         ))}
       </Picker>
-    </View>
   );
 };
 
